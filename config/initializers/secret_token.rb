c0bc319297283a582363dfedb2c6d02d4f3d2bfd
neeraji2it <neeraji2it@gmail.com>
2014-02-22 13:40:12 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestOne::Application.config.secret_key_base = '53db86645152df2f011adf56a7abdb7faa9a60f1ffa0a3ac3813b234cf7504473b56fbe9bcb482eb1d74d6b2c909cc143a7544391fbed20b9634dfaaf247fa63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestOne::Application.config.session_store :cookie_store, key: '_TestOne_session'
